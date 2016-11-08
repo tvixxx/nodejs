9be66b26796249388c858298e6fbdef19333ae31
Kirill Shilov <tvix2000@yandex.ru>
2016-11-08 16:17:59 +0400

    Update fullName.js

--- skb/less2/task1/fullName.js
+++ skb/less2/task1/fullName.js
@@ -1,33 +1,56 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.get('/', (req, res) => {
   const url = req.query;
   const { fullname } = url;
 
   const getConvertedFullName = (fullNameUser) => {
+    var fail = false;
 
-    let maxWords = 4;
-    let invalidPhrase = 'Invalid fullName';
-    let arrayOfUserName = fullNameUser.split(' ');
+    console.log(fullNameUser);
+    const maxWords = 3;
+    const invalidPhrase = 'Invalid fullname';
+    const arrayOfUserName = fullNameUser.split(' ');
+    const re = /[a-zA-Z]+/g;
 
-    if (arrayOfUserName.length >= maxWords) {
+    // q.match(/[a-zA-Z]+/g).length === 1
+
+    const testOnRegEx = (userName) => {
+      if (userName.match(re) === 1 ) {
+        return invalidPhrase;
+      }
+    };
+
+    testOnRegEx(arrayOfUserName[0]);
+
+    if ( (arrayOfUserName.length > maxWords) || (arrayOfUserName == '') ) {
       return invalidPhrase;
     }
 
     let getSurName = arrayOfUserName.pop();
 
-    let arrayOfAbbreviation = arrayOfUserName.map( (word) => word.charAt(0) + '.');
+    let arrayOfAbbreviation = arrayOfUserName.map( (word) => {
+
+      if (testOnRegEx(word)) {
+        console.log(word);
+
+        return fail = true;
+      }
+      return word.charAt(0) + '.';
+    });
+
+    if (fail) {
+      return invalidPhrase;
+    }
 
     return [getSurName, ...arrayOfAbbreviation].join(' ');
   };
 
   const result = getConvertedFullName(fullname);
 
-  // Вывод данных для наглядности
-  res.json({
-    url: url,
-    result: result
-  });
+  res.status(200).send((result).toString());
 });
