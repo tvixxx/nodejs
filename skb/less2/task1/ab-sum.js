c87cb2064a90c88dd2e8680def14792b4a140df4
Kirill Shilov <tvix2000@yandex.ru>
2016-11-08 09:53:00 +0400

    Create ab-sum.js

---
+++ skb/less2/task1/ab-sum.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const url = req.query;
+  const {a, b} = url;
+  const minLetters = 1;
+
+  const sumNumbers = (...values) => {
+    let res = 0;
+
+    for (let i = 0; i < values.length; i++ ) {
+
+      if (isNaN(values[i])) {
+        continue;
+      }
+
+      if (values[i].length > minLetters) {
+        return res = 0;
+      }
+
+      console.log(values);
+      res += parseInt(values[i]);
+    }
+    return res;
+  };
+
+  const result = sumNumbers(a, b);
+  
+  res.json({
+    url: url,
+    result: result
+  });
+});
