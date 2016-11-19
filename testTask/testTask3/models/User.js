83b788a977703a12d63a07de168b30e4e2b7bfce
Kirill Shilov <tvix2000@yandex.ru>
2016-11-19 20:38:42 +0400

    Create User.js

---
+++ testTask/testTask3/models/User.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import _ from 'lodash';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  }
+},
+  { timestamps: true }
+);
+
+UserSchema.methods.toJSON = function() {
+  return _.pick(this, ['name']);
+};
+
+export default mongoose.model('User', UserSchema);
